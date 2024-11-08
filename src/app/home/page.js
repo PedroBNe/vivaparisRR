838a10e62cc018ce4cc5bb07cb1e0aa9ef2db33b
JoaoVitor <roldao0634@gmail.com>
2024-11-08 12:00:17 -0300

    feat:home-taquase

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -8,11 +8,10 @@ import Arrow from "@/assets/Arrow";
 import { Button } from "@/components/ui/button";
 import MyButton from "@/components/buttons";
 import { useEffect, useState } from "react";
-import Imagem from '@/assets/meuteste.jpeg'
+import Imagem2 from '@/assets/meuteste2.jpeg';
 import Image from "next/image";
 import French from "@/assets/Frech";
 import Viagem from '@/assets/viagem.jpeg'
-import Back from '@/assets/eifel.jpeg'; // Background Image 
 import jsonData from '/data.json';
 import Header from "@/components/common/header";
 
@@ -47,80 +46,15 @@ const TravelsCard = ({ title, text, img, color }) => (
     </div>
 );
 
-
-const destination = [
-    {
-        destino: "Museu do Louvre",
-        local: "Paris, França",
-        estilo: "Clássico",
-        img: Imagem
-    },
-    {
-        destino: "Museu do Louvre",
-        local: "Paris, França",
-        estilo: "Egito",
-        img: Imagem
-    },
-    {
-        destino: "Museu do Louvre",
-        local: "Paris, França",
-        estilo: "Kids (em breve)",
-        img: Imagem
-    },
-    {
-        destino: "Museu d’Orsay",
-        local: "Paris, França",
-        img: Imagem
-    },
-    {
-        destino: "Museu Rodin",
-        local: "Paris, França",
-        img: Imagem
-    },
-    {
-        destino: 'Montmartre',
-        local: "Paris, França",
-        rua: 'Île de la Cité + Quartier Latin',
-        img: Imagem
-    },
-    {
-        destino: 'Montmartre',
-        local: "Paris, França",
-        rua: 'Île de la Cité + Le Marais',
-        img: Imagem
-    },
-    {
-        destino: 'Montmartre',
-        local: "Paris, França",
-        rua: 'Eixo histórico',
-        img: Imagem
-    },
-    {
-        destino: 'Montmartre',
-        local: "Paris, França",
-        rua: 'Revolução Francesa (em breve !)',
-        img: Imagem
-    },
-    {
-        destino: 'Castelo de Versalhes',
-        local: 'Versalhes, França',
-        img: Imagem
-    },
-]
-
-const DestinationCard = () => {
+const DestinationCard = ({ destination }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? destination.length - 1 : prevIndex - 1
-        );
-    };
-
     const handleNext = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === destination.length - 1 ? 0 : prevIndex + 1
-        );
+        setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, destination.length - 1));
+    };
+    
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
     };
 
     return(
@@ -142,15 +76,15 @@ const DestinationCard = () => {
                                 <p>{destiny.local}</p>
                             </div>
                         </div>
-                        <Image src={destiny.img} alt={destiny.destino} className="w-[327px] h-[452px] absolute top-2 opacity-90 rounded-xl z-10" />
+                        <Image src={destiny.img} alt={destiny.destino} width={327} height={452} quality={100} className="w-[327px] h-[452px] absolute top-2 opacity-90 rounded-xl z-10" />
                     </div>
                 ))}
             </div>
             <div className="absolute top-[-55px] right-5 flex gap-5">
-                <Button disabled={currentIndex === 0} onClick={handlePrev} size={'largeIcon'} className="flex items-center bg-[#112F38] px-3 rounded-3xl hover:opacity-80 transition ease-in">
+                <Button disabled={currentIndex === 0} onClick={handlePrev} size={'largeIcon'} className={`flex items-center px-3 rounded-3xl bg-[#1E1E1E] hover:opacity-80 transition ease-in`}>
                     <Arrow rotate={180} />
                 </Button>
-                <Button disabled={currentIndex === 5} onClick={handleNext} size={'largeIcon'} className="flex items-center bg-[#112F38] px-3 rounded-3xl hover:opacity-80 transition ease-in">
+                <Button disabled={currentIndex === 5} onClick={handleNext} size={'largeIcon'} className={`flex items-center px-3 rounded-3xl bg-[#1E1E1E] hover:opacity-80 transition ease-in`}>
                     <Arrow />
                 </Button>
             </div>
@@ -162,11 +96,13 @@ const galery = [
     {
         destino: "Museu do Louvre",
         local: "Paris, França",
+        img: Imagem2,
         trips: 34
     },
     {
         destino: "Museu d’Orsay",
         local: "Paris, França",
+        img: Imagem2,
         trips: 34
     },
     {
@@ -189,8 +125,8 @@ const galery = [
 const GaleryCard = () => (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {galery.map((galery, index) => (
-            <div key={index} className="w-[327px] h-[300px] p-2 rounded-xl flex-shrink-0 mx-2 relative text-white">
-                <div className="w-[327px] h-[300px] bg-black text-start rounded-xl p-2 bg-opacity-40 flex flex-col gap-2 items-start justify-end relative z-20">
+            <div key={index} className="w-[350px] h-[350px] p-2 rounded-xl flex-shrink-0 mx-2 relative text-white">
+                <div className="w-[350px] h-[350px] bg-black text-start rounded-xl p-2 bg-opacity-40 flex flex-col gap-2 items-start justify-end relative z-20">
                     <div className="flex flex-col">
                         <h3 className="text-2xl font-bold">{galery.destino}</h3>
                         <p>{galery.local}</p>
@@ -200,7 +136,7 @@ const GaleryCard = () => (
                         {galery.trips}
                     </div>
                 </div>
-                <Image src={galery.img} alt={galery.destino} className="w-[327px] h-[300px] absolute top-2 opacity-90 rounded-xl z-10" />
+                <Image src={galery.img} alt={galery.destino} className="w-[350px] h-[350px] absolute top-2 opacity-90 rounded-xl z-10" />
             </div>
         ))}
     </div>
@@ -208,22 +144,26 @@ const GaleryCard = () => (
 
 export default function Home() {
     const [banner, setBanner] = useState({});
+    const [back, setBack] = useState('');
     const [carousel, setCarousel] = useState([]);
     const [travels, setTravels] = useState([]);
+    const [destiny, setDestiny] = useState([]);
     const [colorPrimary, setColorPrimary] = useState("#f2c249");
+    const [colorSecondary, setColorSecondary] = useState("#0E3D4D");
 
     useEffect(() => {
         setBanner(jsonData[0]?.banner);
+        setBack(jsonData[0]?.banner?.image || '');
         setCarousel(jsonData[0]?.banner?.carousel || []);
         setTravels(jsonData[0]?.travels || []);
+        setDestiny(jsonData[0]?.destiny || []);
         setColorPrimary(jsonData[0]?.colorPrimary ? `#${jsonData[0].colorPrimary.replace('#', '')}` : "#f2c249");
-
-        console.log(banner)
+        setColorSecondary(jsonData[0]?.colorSecondary ? `#${jsonData[0].colorSecondary.replace('#', '')}` : "#0E3D4D");
     }, []);
 
     return(
         <div className="flex flex-col overflow-hidden relative">
-            <Image src={Back} alt="back" width={0} height={0} className="w-full h-[950px] absolute z-10" />
+            <Image src={back} alt="back" width={1000} height={950} quality={100} className="w-full h-[950px] absolute z-10" />
             <Header />
             <div className={`h-[950px] pt-[200px] px-[130px] flex flex-col gap-[110px] text-white z-20 relative bg-black bg-opacity-50`}>
                 <div className="flex flex-col gap-[90px]">
@@ -245,26 +185,26 @@ export default function Home() {
                 </div>
             </div>
             <div className="w-full min-h-[50vh] flex items-end bg-[#1E1E1E] text-[#D9D9D9] relative pb-10">
-                <div className="w-[105%] h-[8vh] bg-[#141B34] flex items-center justify-around absolute left-[-10px] top-0 font-semibold text-3xl rotate-2 z-20">
+                <div className="w-[105%] h-[8vh] flex items-center justify-around absolute left-[-10px] top-0 font-semibold text-3xl rotate-2 z-20" style={{ backgroundColor: colorSecondary }}>
                     <p>explore</p>
                     <p>adventure</p>
                     <p>luxury</p>
                     <p>enjoy</p>
                     <p>explore</p>
                 </div>
-                <div className={`w-full h-[8vh] bg-[${colorPrimary}] absolute top-0 z-10`}></div>
+                <div className="w-full h-[8vh] absolute top-0 z-10" style={{ backgroundColor: colorPrimary }}></div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-10 p-8 px-[130px]">
                     {travels.map((item) => (
                         <TravelsCard key={item.id} title={item.title} text={item.text} img={item.img} color={colorPrimary} />
                     ))}
                 </div>
             </div>
-            <div className="h-[90vh] text-[#0E3D4D] px-[130px] py-[120px] flex flex-col gap-4">
+            <div className="h-[90vh] px-[130px] py-[120px] flex flex-col gap-4" style={{ color: colorSecondary }}>
                 <p className="text-lg font-semibold">Para onde voce gostaria de ir?</p>
                 <h2 className="text-5xl font-bold">Destinos Populares</h2>
-                <DestinationCard />
+                <DestinationCard color={colorSecondary} destination={destiny} />
             </div>
-            <div className="h-[90vh] text-[#0E3D4D] px-[130px] flex flex-col gap-10">
+            <div className="h-[90vh] px-[130px] flex flex-col gap-10" style={{ color: colorSecondary }}>
                 <div className="flex flex-col gap-4">
                     <p className="text-lg font-semibold">Para onde voce gostaria de ir?</p>
                     <div className="flex justify-between">
@@ -294,7 +234,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="h-[100vh] flex justify-center items-start text-[#0E3D4D]">
+            <div className="h-[115vh] flex justify-center items-start" style={{ color: colorSecondary }}>
                 <div className="flex flex-col justify-center items-center gap-4">
                     <p className="text-lg font-semibold">Galeria de viajens</p>
                     <h2 className="text-5xl font-bold">Viajens feitas</h2>
@@ -308,7 +248,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="h-[50vh] flex flex-col gap-[80px] justify-center items-center bg-[#112F38]">
+            <div className="h-[50vh] flex flex-col gap-[80px] justify-center items-center" style={{ backgroundColor: colorSecondary }}>
                 <div className="flex flex-col gap-4 justify-center items-center text-white text-5xl font-bold">
                     <h1>Pronto para viajar?</h1>
                     <p>Entre em contato com a gente</p>
