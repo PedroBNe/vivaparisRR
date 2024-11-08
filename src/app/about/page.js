3ea08568eb2efd77b7bb8c3021a232442a6fde86
JoaoVitor <roldao0634@gmail.com>
2024-11-08 15:21:32 -0300

    feat:agoravai3

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+export default function About() {
+    return(
+        <div className="w-full flex flex-col gap-5 px-[130px]">
+            <div>
+                ola
+            </div>
+            <div>
+                ola2
+            </div>
+        </div>
+    )
+}
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -10,7 +10,7 @@ const header = {
 
 const nav = [
     { link: '/home', text: 'Inicio' },
-    { link: '/home', text: 'Sobre' },
+    { link: '/about', text: 'Sobre' },
     { link: '/home', text: 'Visitas' },
     { link: '/home', text: 'Aulas' },
     { link: '/home', text: 'Na Midia' },
